afed4ed4853724845190a1abcf179d8607691bdf
samvdkamp <sam.vandekamp@student.hu.nl>
2022-04-15 11:54:59 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Utils.euroBedrag(3.11314));
+        System.out.println(Utils.euroBedrag(3.11314, 1));
+    }
+}
